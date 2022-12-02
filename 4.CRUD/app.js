ead51e04a45dbf5877f121797ce1ec2eee763c07
Vijaysai Yekbote <vijay.yekbote@censanext.com>
2022-12-02 14:50:14 +0530

    Added PORT for railway deployement

--- 4.CRUD/app.js
+++ 4.CRUD/app.js
@@ -1,13 +1,16 @@
+import dotenv from "dotenv";
 import express from "express";
 import { productRouter } from "./api/products/products.routes.js";
 import { connectDB } from "./config/db.js";
-
+dotenv.config();
 const main = async () => {
   connectDB();
   const app = express();
   app.use(express.json());
   app.use("/api/products", productRouter);
-  app.listen(3000, () => console.log("Server is up and running at Port 3000"));
+  app.listen(process.env.PORT || 4000, () =>
+    console.log("Server is up and running at Port 3000")
+  );
 };
 
 main();
